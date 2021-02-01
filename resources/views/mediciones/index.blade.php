cb58a81631a2385163e2106af2959b5c4f07e106
carlos noyola <metahuargen@gmail.com>
2021-01-31 18:06:29 -0600

    comit alberto

--- resources/views/mediciones/index.blade.php
+++ resources/views/mediciones/index.blade.php
@@ -132,7 +132,7 @@
 
       <div class="card-header">
         <i class="fa fa-bar-chart-o"></i>
-        <h3 class="box-title">Grafico mensual</h3>  
+        <h3 class="box-title">Medidor </h3>  
 
         <label for="meses">Escoger mes:</label>
         <select name="meses" id="meses">
